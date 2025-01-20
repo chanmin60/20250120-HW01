077771ee76bd1ce30c5b99d16a8b80f077ed2e21
chanmin60 <dlcksals60@gmail.com>
2025-01-20 12:38:57 +0900

    배열 선언 후 입력 받기

---
+++ HW01/HW01/Main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int calculateSum(int numbers[], int size)
+{
+	int sum = 0;
+	for (int i = 0; i < size; i++)
+	{
+		sum += numbers[i];
+	}
+	return sum;
+}
+
+double calculateAverage(int numbers[], int size)
+{
+	double sum = calculateSum(numbers, size);
+	double average = sum / size;
+	return average;
+}
+
+int main()
+{
+	const int SIZE = 5;
+	int numbers[SIZE];
+	
+	cout << "다섯 숫자를 입력하세요\n";
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << i+1 << "번째 숫자 : ";
+		cin >> numbers[i];
+	}
+	int total = calculateSum(numbers, SIZE);
+	int average = calculateAverage(numbers, SIZE);
+	cout << "다섯 숫자의 합계는 : " << total<<"\n";
+	cout << "다섯 숫자의 평균은 : " << average << "\n";
+	return 0;
+}
